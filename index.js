ebf98525b425b6129631cffb74a67cdc58e9cd33
Pablo Canseco <pablocanseco2014@gmail.com>
2025-03-30 00:27:08 -0300

    clase 1 cariables y tipos de datos

---
+++ index.js
@@ -0,0 +1,57 @@
+//console.log("hola mundo")
+/*
+ggggg
+gggggg */
+
+
+
+let nombre = "hola"
+console.log("🚀 ~ nombre:", nombre)
+let edad 
+console.log("🚀 ~ edad:", edad)
+dni = 12345555
+console.log(dni)
+
+edad = 40
+edad= 2344555
+console.log(edad)
+
+const pi = 3.14
+
+let srt = "hola equipo"
+
+console.log(typeof {}); // "object" (un objeto común)
+console.log(typeof []); // "object" (un array es técnicamente un objeto en JavaScript)
+console.log(typeof function(){}); // "function" (las funciones tienen su propio tipo)
+
+console.log(typeof srt == "string")
+
+let bool = false
+
+if(bool){
+    console.log("verdadero")
+}
+
+let nulo = null 
+console.log(nulo)
+
+const persona= {nombre : "pablo ",
+  apellido : "canseco" ,
+   calcular: function(){
+     return 2+2 ;
+   }
+   
+}
+
+
+
+
+persona.dni = 23014889
+
+
+
+console.log(typeof(persona.nombre))
+
+const array = ["objeto1", [1,2,3], {}, 1]
+
+console.log(array[1][2])
