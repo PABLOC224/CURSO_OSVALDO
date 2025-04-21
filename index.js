71d5efd81848d8f839824e5c5e241f393ddd9a7f
Pablo Canseco <pablocanseco2014@gmail.com>
2025-04-21 01:28:19 -0300

    clase4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  
 const persona = require("./utils/persona")
-const {array1,array2} = require("./utils/arrays")
+const {array2,array3} = require("./utils/arrays")
 
 const data = ["pablo", "canseco"]
 
@@ -9,8 +9,8 @@ const[posicion1, posicion2] = data
  console.log(posicion1)
  console.log(posicion2)
 
- console.log(array1)
-
+ console.log(array3)
+ console.log(array2)
 
  console.log("----------------")
 
@@ -26,3 +26,46 @@ const[posicion1, posicion2] = data
  const{edad, nombre, direccion} = persona2
 
  console.log(edad, nombre, direccion.pais)
+
+ // metodos 
+
+ console.log(array3.toString())
+ console.log(array3.length)
+ //const arrayFinal = JSON.parse(JSON.stringify(array3))
+// console.log(arrayFinal)
+ 
+const arrayFinal = []
+console.log(arrayFinal)
+
+function ciclar(elemento) {
+       console.log(elemento)
+
+}
+
+array3.forEach(ciclar
+);
+
+let tareas = ["Lavar los platos", "Hacer ejercicio", "Leer un libro", "Pagar cuentas"];
+
+const tareasEliminadas = tareas.splice(2,1)
+
+console.log(tareas)
+
+tareas.splice(2,0,"ir de compras")
+
+console.log(tareas)
+
+tareas.splice(tareas.length , 0 , "ordenar armario")
+
+console.log(tareas)
+console.log(tareasEliminadas)
+
+const numeros = [7, 2, 9, 4, 1, 8, 3, 6, 10, 5];
+
+function restar(a,b) {
+      return  a-b
+}
+
+numeros.sort(restar)  
+
+console.log(numeros)
--- utils/arrays.js
+++ utils/arrays.js
@@ -1,10 +1,10 @@
-const array1 = [1,2,3,4]
+const array3 = [1,2,3,4]
 const array2 = [3,5,6,9]
 
-const objeto = 
+
 
 module.exports = {
-  array1 ,
+  array3 ,
   array2 
 
 }
